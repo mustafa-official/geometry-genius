c3f503a695146fdf88f3d62291fc5433c3da97b5
webdev-mostofa <mdmostofaa24@gmail.com>
2024-02-13 00:43:03 +0600

    trianle done

---
+++ js/triangle.js
@@ -0,0 +1,29 @@
+
+
+const calculation = document.getElementById('calculation');
+
+
+
+function calculateTriangle() {
+    // get triangleBase
+    const triangleBase = document.getElementById('triangle-base').value;
+    const base = parseFloat(triangleBase);
+
+
+    // get triangle-height
+    const triangleHeight = document.getElementById('triangle-height').value;
+    const height = parseFloat(triangleHeight);
+
+    // do calculation 
+    const result = 0.5 * base * height;
+    // display calculation
+    const calculation = document.getElementById('calculation');
+    calculation.innerText = result;
+
+
+
+
+
+
+
+}
